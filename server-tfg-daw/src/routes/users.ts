40703e3b010d1b5fba697cc09341f86f88bb7036
Diego <diego.davilafreitas@gmail.com>
2023-10-28 20:40:49 +0200

    adding updateuser

--- server-tfg-daw/src/routes/users.ts
+++ server-tfg-daw/src/routes/users.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { checkAdmin } from "../utils/roles";
-import { createUser, deleteUser, getUsers } from "../controllers/users";
+import { createUser, deleteUser, getUsers, updateUser } from "../controllers/users";
 
 export const usersRouter = Router();
 
 usersRouter.get("/", checkAdmin, getUsers);
 usersRouter.post("/", checkAdmin, createUser);
 usersRouter.delete("/:id", checkAdmin, deleteUser);
+usersRouter.update("/:id", checkAdmin, updateUser);
