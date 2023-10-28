4f8d64cb6d565e8bdd26e2ed7860dfc9cfbf58b8
Diego <diego.davilafreitas@gmail.com>
2023-10-28 21:22:26 +0200

    fix put method on updateUser endpoint

--- server-tfg-daw/src/routes/users.ts
+++ server-tfg-daw/src/routes/users.ts
@@ -7,4 +7,4 @@ export const usersRouter = Router();
 usersRouter.get("/", checkAdmin, getUsers);
 usersRouter.post("/", checkAdmin, createUser);
 usersRouter.delete("/:id", checkAdmin, deleteUser);
-usersRouter.update("/:id", checkAdmin, updateUser);
+usersRouter.put("/:id", checkAdmin, updateUser);
