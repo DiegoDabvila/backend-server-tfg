35155081f5d96a8d3135ce53c5cf22dd24ba7d35
Diego <diego.davilafreitas@gmail.com>
2023-11-19 22:18:33 +0100

    feat(admin): fix password change in updateUser endpoint

--- server-tfg-daw/src/controllers/users.ts
+++ server-tfg-daw/src/controllers/users.ts
@@ -44,13 +44,17 @@ export const deleteUser = async (req: Request, res: Response) => {
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const {name, username, surnames, isAdmin} = req.body;
-
+  const {name, username, surnames, isAdmin, password} = req.body;
+  const cryptoPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
   const updated = await prisma.user.update({
     data: {
       name,
       username,
       surnames,
+      password: cryptoPassword,
       isAdmin
     },
     where: { id: parseInt(req.params.id) },
